05f2efc9d9b1cd19558456a3eb30677d02d1de96
Umang Tanwar <156454079+umangtanwar@users.noreply.github.com>
2025-02-18 16:18:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       height: "100vh",
       backgroundColor: "#f4f4f4"
     }}>
-      <h1 style={{ color: "#333" }}>Welcome to Our University Portal</h1>
+      <h1 style={{ color: "#333" }}>Hello My name is Umang</h1>
       <p style={{ color: "#666" }}>This is a simple base page hosted on AWS.</p>
     </div>
   );
